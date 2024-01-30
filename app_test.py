e48fd9d743127177f190858e5fdae6ea5ac25fa4
noascape <nheim200@gmail.com>
2024-01-30 14:23:58 +0100

    Man kann jetzt Lebensmittel eintragen und so eine Liste erstellen und diese Bearbeiten

--- app_test.py
+++ app_test.py
@@ -1,46 +1,114 @@
+from typing import Any, List, Optional, Union
 import flet as ft
+from flet_core.control import Control, OptionalNumber
+from flet_core.ref import Ref
+from flet_core.types import AnimationValue, ClipBehavior, OffsetValue, ResponsiveNumber, RotateValue, ScaleValue
+
+class GourmetApp(ft.UserControl):
+    def build(self):
+        self.new_meal = ft.TextField(hint_text="Was hast du noch in der Vorratskammer?", expand=True)
+        self.meals = ft.Column()
+
+        # application's root control (i.e. "view") containing all other controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_meal,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.meals,
+            ],
+        )
+
+    def add_clicked(self, e):
+        meal = Meal(self.new_meal.value, self.meal_delete)
+        self.meals.controls.append(meal)
+        self.new_meal.value = ""
+        self.update()
+
+    def meal_delete(self, meal):
+        self.meals.controls.remove(meal)
+        self.update()
+
+
+
+class Meal(ft.UserControl):
+    def __init__(self, meal_name, meal_delete):
+        super().__init__()
+        self.meal_name = meal_name
+        self.meal_delete = meal_delete
+
+    def build(self):
+        self.display_meal = ft.Checkbox(value=False, label=self.meal_name)
+        self.edit_name = ft.TextField(expand=1)
+
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_meal,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.icons.CREATE_OUTLINED,
+                            tooltip="Bearbeiten",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            ft.icons.DELETE_OUTLINE,
+                            tooltip="Löschen",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.colors.GREEN,
+                    tooltip="Bestätigen",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        return ft.Column(controls=[self.display_view, self.edit_view])
+
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_meal.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_meal.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def delete_clicked(self, e):
+        self.meal_delete(self)
+
 
 def main(page: ft.Page):
     page.title = "Gourmet Guide"
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    first_name = ft.TextField(label="First name", autofocus=True)
-    last_name = ft.TextField(label="Last name")
-    greetings = ft.Column()
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
 
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+    # create application instance
+    meal = GourmetApp()
 
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
-    def btn_click(e):
-        greetings.controls.append(ft.Text(f"Hello, {first_name.value} {last_name.value}!"))
-        first_name.value = ""
-        last_name.value = ""
-        page.update()
-        first_name.focus()
-
-    page.add(
-        first_name,
-        last_name,
-        ft.ElevatedButton("Say hello!", on_click=btn_click),
-        greetings,
-    )
-
-    page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
-    )
+    # add application's root control to the page
+    page.add(meal)
 
 ft.app(target=main)
-
---
+++ usefull_snippets.md
@@ -0,0 +1,44 @@
+#flet.app(target=main, view=flet.WEB_BROWSER)
+
+
+def main(page: ft.Page):
+    page.title = "Gourmet Guide"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    first_name = ft.TextField(label="First name", autofocus=True)
+    last_name = ft.TextField(label="Last name")
+    greetings = ft.Column()
+
+    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+
+    def minus_click(e):
+        txt_number.value = str(int(txt_number.value) - 1)
+        page.update()
+
+    def plus_click(e):
+        txt_number.value = str(int(txt_number.value) + 1)
+        page.update()
+
+    def btn_click(e):
+        greetings.controls.append(ft.Text(f"Hello, {first_name.value} {last_name.value}!"))
+        first_name.value = ""
+        last_name.value = ""
+        page.update()
+        first_name.focus()
+
+    page.add(
+        first_name,
+        last_name,
+        ft.ElevatedButton("Say hello!", on_click=btn_click),
+        greetings,
+    )
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment=ft.MainAxisAlignment.CENTER,
+        )
+    )
